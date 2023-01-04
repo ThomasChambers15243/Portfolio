a8f78fadabba355d77a4b196721f54fcfe7dcd76
TomChambers <tc262389@falmouth.ac.uk>
2023-01-04 13:59:12 +0000

    Added sql select queries

--- Main.session.sql
+++ Main.session.sql
@@ -1,4 +1,4 @@
---@block Create tables
+--@block Create tables content, contactme and projects
 CREATE TABLE Content(
     id INTEGER PRIMARY KEY IDENTITY(1, 1),
     articleLocation VARCHAR(255),
@@ -15,10 +15,20 @@ CREATE TABLE ContactMe(
 
 CREATE TABLE Projects(
     id INT PRIMARY KEY IDENTITY(1, 1),
+    repoType VARCHAR(255),
+    CONSTRAINT chk_repoType CHECK (repoType IN ("github","zip","pdf")),
+    projectName VARCHAR(255),
+    projectDescription TEXT,
+    projectLink TEXT,    
 );
 
+--@block
+-- Example SQL Queries
+-- Gets all rows from contactme table
+SELECT * FROM ContactMe;
 
---@block Add Content
-INSERT INTO Content VALUES(
+-- Gets all articles from the content table but only from the index page
+SELECT * FROM Content WHERE articleLocation = "index";
 
-);
+-- Get the name and link to any project where the repo type is not a zip and it has a valid link
+SELECT projectName, projectLink FROM Projects WHERE repoType != "zip" AND projectLink IS NOT NULL;
--- Pages/contactMe.php
+++ Pages/contactMe.php
@@ -1,3 +1,26 @@
 <?php 
-echo $_POST['name']; 
+$servername = "localhost";
+$username = "root";
+$password = "pass";
+$dbname = "main";
+
+// Create connection
+$conn = new mysqli($servername,
+    $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: "
+        . $conn->connect_error);
+}
+
+$sqlquery = "INSERT INTO ContactMe VALUES
+    ($_POST["name"], $_POST["email"], $_POST["message"])"
+
+if ($conn->query($sql) === TRUE) {
+    echo "record inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 ?>.
